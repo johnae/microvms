f24a524d0e5428dd5b4557688c5c590e0fc39ad1
John Axel Eriksson <john@insane.se>
2024-05-11 11:39:35 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -24,16 +24,21 @@
       inherit system;
       modules = [
         microvm.nixosModules.microvm
-        {
+        ({pkgs, ...}: {
           networking.hostName = "my-microvm";
           networking.firewall.allowedUDPPorts = [67];
           systemd.network.enable = true;
           systemd.network.networks."10-lan" = {
             matchConfig.Name = "enp*";
-            address = [
-              "10.100.0.2/24"
-            ];
+            networkConfig.DHCP = "ipv4";
+            # address = [
+            #   "10.100.0.2/27"
+            # ];
+            # routes = [
+            #   {routeConfig.Gateway = "10.100.0.1";}
+            # ];
           };
+          services.tailscale.enable = true;
           users.users.root.hashedPassword = "$6$d6erGtUBlhIR/WaN$N5TwjLxlY1nD.neCyd5h7VGO3jqXUfQg6ZWbuc9ByLQvL3LT/15hX992bLAdP46enllfF2LiBcx4IvObMlp1p/";
           users.users.root.openssh.authorizedKeys.keys = [
             "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyjMuNOFrZBi7CrTyu71X+aRKyzvTmwCEkomhB0dEhENiQ3PTGVVWBi1Ta9E9fqbqTW0HmNL5pjGV+BU8j9mSi6VxLzJVUweuwQuvqgAi0chAJVPe0FSzft9M7mJoEq5DajuSiL7dSjXpqNFDk/WCDUBE9pELw+TXvxyQpFO9KZwiYCCNRQY6dCjrPJxGwG+JzX6l900GFrgOXQ3KYGk8vzep2Qp+iuH1yTgEowUICkb/9CmZhHQXSvq2gAtoOsGTd9DTyLOeVwZFJkTL/QW0AJNRszckGtYdA3ftCUNsTLSP/VqYN9EjxcMHQe4PGjkK7VLb59DQJFyRQqvPXiUyxNloHcu/sDuiKHIk/0qDLHlVn2xc5zkvzSqoQxoXx+P4dDbje1KHLY8E96gLe2Csu0ti+qsM5KEvgYgwWwm2g3IBlaWwgAtC0UWEzIuBPrAgPd5vi+V50ITIaIk6KIV7JPOubLUXaLS5KW77pWyi9PqAGOXj+DgTWoB3QeeZh7CGhPL5fAecYN7Pw734cULZpnw10Bi/jp4Nlq1AJDk8BwLUJbzZ8aexwMf78syjkHJBBrTOAxADUE02nWBQd0w4K5tl/a3UnBYWGyX8TD44046Swl/RY/69PxFvYcVRuF4eARI6OWojs1uhoR9WkO8eGgEsuxxECwNpWxR5gjKcgJQ=="
@@ -41,6 +46,18 @@
             "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAwJWtQ5ZU9U0szWzJ+/GH2uvXZ15u9lL0RdcHdsXM0VAAAABHNzaDo="
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK+trOinD68RD1efI6p05HeaNA0SjzeRnUvpf22+jsq+"
           ];
+          environment.systemPackages = with pkgs; [
+            zellij
+            git
+            helix
+          ];
+          users.users.john = {
+            uid = 1337;
+            shell = pkgs.nushell;
+            isNormalUser = true;
+            hashedPassword = "$6$d6erGtUBlhIR/WaN$N5TwjLxlY1nD.neCyd5h7VGO3jqXUfQg6ZWbuc9ByLQvL3LT/15hX992bLAdP46enllfF2LiBcx4IvObMlp1p/";
+            extraGroups = ["wheel" "docker" "video" "audio" "kvm" "libvirtd"];
+          };
           services.openssh = {
             enable = true;
             settings.PermitRootLogin = "yes";
@@ -66,6 +83,8 @@
             ];
             socket = "control.socket";
             hypervisor = "cloud-hypervisor";
+            vcpu = 4;
+            mem = 16384;
             interfaces = [
               {
                 type = "tap";
@@ -74,7 +93,7 @@
               }
             ];
           };
-        }
+        })
       ];
     };
   };
